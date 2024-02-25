872ce8fc9d0910b9ab5fc55566fc5fe035f2b55a
Jumpei Yamaguchi <28538293+nmoa@users.noreply.github.com>
2024-02-25 18:33:41 +0900

    update main.py

--- main.py
+++ main.py
@@ -14,58 +14,61 @@ def update_sheet(sheet: gspread.Worksheet, df_new: pd.DataFrame):
     if records:
         df_current = pd.DataFrame(records)
         # df_currentからdf_newにある日付を除外する
-        df_current_dropped = df_current[~df_current['日付'].isin(
-            df_new['日付'].unique())]
+        df_current_dropped = df_current[
+            ~df_current["日付"].isin(df_new["日付"].unique())
+        ]
         # df_currentをdf_newに更新する。indexがなかったら追加する
         df_paste = pd.concat([df_current_dropped, df_new])
     else:
         df_paste = df_new
 
     sheet.clear()
-    sheet.update('A1', [df_paste.columns.values.tolist()] +
-                 df_paste.values.tolist())
+    sheet.update("A1", [df_paste.columns.values.tolist()] + df_paste.values.tolist())
 
 
-def update(args, date_since: datetime.date = None):
+def update(ss_name: str, cookie_path: str, date_since: datetime.date = None):
     """指定した月から現在までの収入・支出をスプレッドシートに更新する
 
     Args:
         date (datetime.date, optional): 月。指定しない場合は今月を指定する.
     """
-    if (not os.environ['MF_EMAIL']) or (not os.environ['MF_PASSWORD']):
-        logger.error('MF_EMAIL or MF_PASSWORD is not set.')
-        return
+    if (not os.environ["MF_EMAIL"]) or (not os.environ["MF_PASSWORD"]):
+        logger.error("MF_EMAIL or MF_PASSWORD is not set.")
+        return False
 
     if date_since is None:
         # 今日から2ヶ月前を指定
         date_since = datetime.date.today() - datetime.timedelta(days=60)
 
-    mf = MoneyforwardDriver(args.cookie_path)
-    mf.login()
+    mf = MoneyforwardDriver(cookie_path)
+    login_success = mf.login()
+
+    if not login_success:
+        logger.error("Login failed.")
+        return
 
     [income_new, expense_new] = mf.fetch_monthly_income_and_expenses_since(
-        date_since.year, date_since.month)
+        date_since.year, date_since.month
+    )
 
     gc = gspread.service_account()
-    ss = gc.open(args.ss_name)
-    expense_sheet = ss.worksheet('支出')
+    ss = gc.open(ss_name)
+    expense_sheet = ss.worksheet("支出")
     update_sheet(expense_sheet, expense_new)
 
-    income_sheet = ss.worksheet('収入')
+    income_sheet = ss.worksheet("収入")
     update_sheet(income_sheet, income_new)
 
 
-if __name__ == '__main__':
-    """ This is executed when run from the command line """
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Required positional argument
     parser.add_argument("ss_name", help="Name of the destination spreadsheet")
 
     # Optional argument flag which defaults to False
-    parser.add_argument("-c", "--cookie-path",
-                        action="store", dest="cookie_path")
+    parser.add_argument("-c", "--cookie-path", action="store", dest="cookie_path")
 
     args = parser.parse_args()
 
-    update(args)
+    update(args.ss_name, args.cookie_path, date_since=datetime.date(2023, 9, 1))
